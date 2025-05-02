dbe96545a6f8592a0cac1b9f7465ba9f80172c78
Lehlonolo Mokoena <126181969+lehlonolo27@users.noreply.github.com>
2025-05-02 20:49:53 +0200

    Created the Expenses Page with View + Add Form

--- NSFASBudgetTracker/NSFASBudgetTracker.Client/obj/Debug/net8.0/NSFASBudgetTracker.Client.AssemblyInfo.cs
+++ NSFASBudgetTracker/NSFASBudgetTracker.Client/obj/Debug/net8.0/NSFASBudgetTracker.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NSFASBudgetTracker.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97a05b2648a98203efacfb8c447ec259f49af2f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83b0dec7021f3a08eb216df1ef045b3a390c04bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("NSFASBudgetTracker.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NSFASBudgetTracker.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
