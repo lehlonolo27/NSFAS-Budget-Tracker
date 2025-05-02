f08996a100f17c51d2001b59245183948442922f
Lehlonolo Mokoena <126181969+lehlonolo27@users.noreply.github.com>
2025-05-02 14:55:56 +0200

    Made changes to Expense Api

--- NSFASBudgetTracker/NSFASBudgetTracker.API/obj/Debug/net8.0/NSFASBudgetTracker.API.AssemblyInfo.cs
+++ NSFASBudgetTracker/NSFASBudgetTracker.API/obj/Debug/net8.0/NSFASBudgetTracker.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NSFASBudgetTracker.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0287d225731e80f1029938081c1181ccd719e23d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4f20dc8cfe0d8eb3eaf5edc2a06fe0e3479aca7")]
 [assembly: System.Reflection.AssemblyProductAttribute("NSFASBudgetTracker.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NSFASBudgetTracker.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
